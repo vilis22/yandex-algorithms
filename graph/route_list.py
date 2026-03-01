05dc962bd9ee5ac5ad65630226594af20fff3e64
hp <vitaly.22.84@gmail.com>
2026-03-01 18:55:44 +0700

    fix

--- graph/route_list.py
+++ graph/route_list.py
@@ -1,15 +1,29 @@
 def main():
     n, m = map(int, input().split())
-    adjacency_matrix_1 = [[0 for _ in range(n)] for _ in range(n)]
+    matrix_1 = [[0 for _ in range(n)] for _ in range(n)]
+    matrix_2 = [[0 for _ in range(n)] for _ in range(n)]
 
     for _ in range(m):
-        stops = list(map(int, input().split()))
-        for i in range(1, stops[0]):
-            adjacency_matrix_1[stops[i] - 1][stops[i + 1] - 1] = 1
-            adjacency_matrix_1[stops[i + 1] - 1][stops[i] - 1] = 1
+        data = list(map(int, input().split()))
+        k = data[0]
+        route = data[1:]
+
+        for i in range(k - 1):
+            u, v = route[i], route[i + 1]
+            matrix_1[u - 1][v - 1] = 1
+            matrix_1[v - 1][u - 1] = 1
+
+        for i in range(k):
+            for j in range(i + 1, k):
+                u, v = route[i], route[j]
+                matrix_2[u - 1][v - 1] = 1
+                matrix_2[v - 1][u - 1] = 1
 
     for _ in range(n):
-        print(*adjacency_matrix_1[_])
+        print(*matrix_1[_])
+
+    for _ in range(n):
+        print(*matrix_2[_])
 
 
 if __name__ == "__main__":
